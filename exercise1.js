137c81bae71bd5b32f61e9bfa10042dc38b303eb
Marina Petrenko <yermolina31226@gmail.com>
2024-11-14 15:02:32 +0200

    targ

--- exercise1.js
+++ exercise1.js
@@ -2,3 +2,7 @@ let myname = 'Marina'
 console.log('Marina')
 let mysecondname = 'Petrenko'
 console.log("Petrenko")
+let age = 47
+console.log(47)
+const birthdate = '9.6.1977'
+console.log('9.6.1977')
